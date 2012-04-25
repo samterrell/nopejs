4a01777566f73d7c3cd0e87dabcd4bb04f1dc295
Sam <samterrell@gmail.com>
2012-04-24 21:40:41 -0400

    Refactoring proxy, since protocol and server are required, path is the only optional parameter, and there is a clear ordering to the options, I opted to make them parameters instead of options.

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ var string = require('./servlets/string');
 var static = require('./servlets/static');
 
 module.exports = {
-    'twitter':proxy({protocol:https, server:"api.twitter.com"}),
+    'twitter':proxy(https, "api.twitter.com"),
     '':string("Hello World"),
     'test':{
         'sam':string("Hello Sam")
--- servlets/proxy.js
+++ servlets/proxy.js
@@ -1,7 +1,7 @@
-module.exports = function (url) {
+module.exports = function (protocol, server, path) {
     return function (req, resp) {
-        req.path.unshift(url.path || '');
-        url.protocol.get({'host':url.server, 'path':req.path.join('/')},
+        req.path.unshift(path || '');
+        protocol.get({'host':server, 'path':req.path.join('/')},
             function (result) {
                 resp.setHeader('content-type', result.headers['content-type']);
                 result.on('data', function (data) {
